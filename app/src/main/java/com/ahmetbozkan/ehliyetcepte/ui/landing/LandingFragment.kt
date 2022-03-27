3f19cb959a581b5b6c5bd48bc7a41f94fb77f0fb
ahmetbozkan <ahmetbozkan@altamira.com.tr>
2022-03-27 23:08:14 +0300

    Exams json file imported, LandingPage (start destination) created

---
+++ app/src/main/java/com/ahmetbozkan/ehliyetcepte/ui/landing/LandingFragment.kt
@@ -0,0 +1,20 @@
+package com.ahmetbozkan.ehliyetcepte.ui.landing
+
+import android.os.Bundle
+import androidx.fragment.app.viewModels
+import com.ahmetbozkan.ehliyetcepte.R
+import com.ahmetbozkan.ehliyetcepte.base.BaseFragment
+import com.ahmetbozkan.ehliyetcepte.databinding.FragmentLandingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LandingFragment: BaseFragment<FragmentLandingBinding, LandingViewModel>() {
+
+    override fun getLayoutId(): Int = R.layout.fragment_landing
+
+    override val viewModel: LandingViewModel by viewModels()
+
+    override fun initialize(savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/ahmetbozkan/ehliyetcepte/ui/landing/LandingViewModel.kt
@@ -0,0 +1,9 @@
+package com.ahmetbozkan.ehliyetcepte.ui.landing
+
+import com.ahmetbozkan.ehliyetcepte.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LandingViewModel @Inject constructor() : BaseViewModel() {
+}
