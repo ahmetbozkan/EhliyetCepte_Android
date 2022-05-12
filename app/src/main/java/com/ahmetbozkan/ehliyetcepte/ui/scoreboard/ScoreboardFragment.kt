87fb7b6e011b21da3006ecc15220231ef55a8d96
ahmetbozkan <ahmetbozkan@altamira.com.tr>
2022-05-13 00:07:36 +0300

    Scoreboard to display questions navigation completed

--- app/src/main/java/com/ahmetbozkan/ehliyetcepte/ui/scoreboard/ScoreboardFragment.kt
+++ app/src/main/java/com/ahmetbozkan/ehliyetcepte/ui/scoreboard/ScoreboardFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.fragment.app.viewModels
 import com.ahmetbozkan.ehliyetcepte.R
 import com.ahmetbozkan.ehliyetcepte.base.BaseFragment
+import com.ahmetbozkan.ehliyetcepte.data.model.exam.ExamWithQuestions
 import com.ahmetbozkan.ehliyetcepte.data.model.result.ExamWithQuestionsAndResult
 import com.ahmetbozkan.ehliyetcepte.databinding.FragmentScoreboardBinding
+import com.ahmetbozkan.ehliyetcepte.util.Event
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -29,12 +31,24 @@ class ScoreboardFragment : BaseFragment<FragmentScoreboardBinding, ScoreboardVie
 
     private fun observeLiveData() {
         viewModel.results.observe(viewLifecycleOwner, ::observeResults)
+        viewModel.examWithQuestions.observe(viewLifecycleOwner, ::observeExamWithQuestions)
     }
 
     private fun observeResults(list: List<ExamWithQuestionsAndResult>) {
         scoreboardAdapter.submitList(list)
     }
 
+    private fun observeExamWithQuestions(result: Event<ExamWithQuestions>) {
+        result.getContentIfNotHandled()?.let {
+            val action = ScoreboardFragmentDirections
+                .actionScoreboardFragmentToDisplayQuestionResultFragment(
+                    0,
+                    result.peekContent()
+                )
+
+            navigate(action)
+        }
+    }
 
     private fun setScoreboardRecyclerView() {
         binding.rcvScoreboard.apply {
@@ -44,7 +58,7 @@ class ScoreboardFragment : BaseFragment<FragmentScoreboardBinding, ScoreboardVie
 
         scoreboardAdapter.click = object : (ExamWithQuestionsAndResult) -> Unit {
             override fun invoke(entity: ExamWithQuestionsAndResult) {
-                // todo: navigate to display results page
+                viewModel.onScoreClicked(entity.exam.examId)
             }
 
         }
